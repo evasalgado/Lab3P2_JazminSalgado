c3a27a9be57d35cdfd2fb5ea3e3d9c91318c9945
evasalgado <imviarsha@gmail.com>
2023-10-27 13:14:23 -0600

    Creacion de proyecto primer commit

---
+++ Lab3P2_JazminSalgado/src/lab3p2_jazminsalgado/Lab3P2_JazminSalgado.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab3p2_jazminsalgado;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author evaja
+ */
+public class Lab3P2_JazminSalgado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        
+        int op=0;
+        do {
+            System.out.println("----MENU----\n"
+                    + "1. "
+                    + "2."
+                    + "3."
+                    + "4."
+                    + "5. ");
+        } while (op!=5);
+    }
+    
+}
